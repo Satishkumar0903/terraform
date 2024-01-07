e0109d40b690d801bb0ac92350a9f48012536e33
unknown <bsatheesh779@gmail.com>
2024-01-07 14:29:16 +0530

    terraform

--- for_each/provider.tf
+++ for_each/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.31.0" # AWS provider version, not terraform version
     }
   }
+
+    backend "s3" {
+    bucket = "devops-practice-state"
+    key    = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "devops-practice-lock"
+  }
 }
 
 provider "aws" {
